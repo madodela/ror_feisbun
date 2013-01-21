b19a2ae720edeef08e9feb5ac73f6f542a2e2a32
Loli Delgado <madodela@gmail.com>
2013-01-21 16:34:20 -0600

    starting project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorFeisbun::Application.config.secret_token = 'e14ac40432cb488c4b3ba724fd43a115962758463728edbf05d3f747f32aee0f5b845d3a7c56a33301652606a167b90389476a535a491de1ebb2b401e1a6ca80'
